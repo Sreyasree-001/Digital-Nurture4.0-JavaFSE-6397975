042dd489401bab911f033b9d53209a77088fe121
Sreyasree-001 <sreyasree202@gmail.com>
2025-06-28 20:28:06 +0530

    week-2 submissions

--- Week-2_TDD using JUnit5 and Mockito/JUnit_Basic Testing Exercises/3_Assertions in JUnit/Code/src/test/java/org/code/AssertionsTest.java
+++ Week-2_TDD using JUnit5 and Mockito/JUnit_Basic Testing Exercises/3_Assertions in JUnit/Code/src/test/java/org/code/AssertionsTest.java
@@ -1,8 +1,9 @@
 package org.code;
 
-import org.junit.Test;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AssertionsTest {
     @Test
