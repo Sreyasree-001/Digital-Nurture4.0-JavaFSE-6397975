0ffa5e1a973ecb58af83a387da7b5f87e82217c7
Sreyasree-001 <sreyasree202@gmail.com>
2025-07-07 18:08:42 +0530

    week-3 submissions

---
+++ Week-3_Spring Data JPA with Spring Boot, Hibernate/Hands-on-1_Spring Data JPA - Quick Example /Code/orm-learn/src/main/java/com/cognizant/orm_learn/service/CountryService.java
@@ -0,0 +1,28 @@
+package com.cognizant.orm_learn.service;
+
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.repository.CountryRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CountryService {
+    private CountryRepository countryRepository;
+
+    @Autowired
+    public CountryService(CountryRepository countryRepository){
+        this.countryRepository = countryRepository;
+    }
+
+    public CountryService(){}
+
+    @Transactional
+    public List<Country> getAllCountries(){
+        return countryRepository.findAll();
+    }
+
+
+}
